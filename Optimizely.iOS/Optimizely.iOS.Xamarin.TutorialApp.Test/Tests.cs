6d80290858b21691cdf827b6deacad30028a05bc
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-08-20 17:41:41 +0300

    Added project for tests.

---
+++ Optimizely.iOS/Optimizely.iOS.Xamarin.TutorialApp.Test/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Optimizely.iOS.Xamarin.TutorialApp.Test
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    public void Pass ()
+    {
+      Assert.True (true);
+    }
+
+    [Test]
+    public void Fail ()
+    {
+      Assert.False (true);
+    }
+
+    [Test]
+    [Ignore ("another time")]
+    public void Ignore ()
+    {
+      Assert.True (false);
+    }
+  }
+}
