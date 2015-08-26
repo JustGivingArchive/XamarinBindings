a053e78e80c2ca1a839d93f1a8670cc2f823a9cc
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-08-26 15:20:58 +0300

    kiss metrics tests

--- KissMetrics.iOS/KissMetrics.iOS.Tests/KissMetricsSDKTests.cs
+++ KissMetrics.iOS/KissMetrics.iOS.Tests/KissMetricsSDKTests.cs
@@ -1,195 +1,278 @@
-﻿//using System;
-//using NUnit.Framework;
-//using Foundation;
-//using KissMetricsiOS;
-//
-//namespace KissMetrics.iOS.Tests
-//{
-//  [TestFixture]
-//  public class KissMetricsSDKTests
-//  {
-//    readonly KISSmetricsAPI instance = KISSmetricsAPI.SharedAPI;
-//
-//    [Test]
-//    public void SharedAPIWithKey()
-//    {
-//      try
-//      {
-//        KISSmetricsAPI.SharedAPIWithKey("apiKey");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void Identify()
-//    {
-//      try
-//      {
-//        instance.Identify("identity");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void ClearIdentity()
-//    {
-//      try
-//      {
-//        instance.ClearIdentity();
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void Alias()
-//    {
-//      try
-//      {
-//        instance.Alias("Fidentity", "Sidentity");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void Record()
-//    {
-//      try
-//      {
-//        instance.Record("name");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void RecordWithProperties()
-//    {
-//      try
-//      {
-//        instance.Record("name", new NSDictionary());
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void RecordEvent()
-//    {
-//      try
-//      {
-//        instance.RecordEvent("name", new NSDictionary());
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void RecordOnce()
-//    {
-//      try
-//      {
-//        instance.RecordOnce("name");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void SetDistinct()
-//    {
-//      try
-//      {
-//        instance.SetDistinct(new NSObject(), "propertyKey");
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void AutoRecordAppLifecycle()
-//    {
-//      try
-//      {
-//        instance.AutoRecordAppLifecycle();
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void AutoRecordInstalls()
-//    {
-//      try
-//      {
-//        instance.AutoRecordInstalls();
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void AutoSetHardwareProperties()
-//    {
-//      try
-//      {
-//        instance.AutoSetHardwareProperties();
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//
-//    [Test]
-//    public void AutoSetAppProperties()
-//    {
-//      try
-//      {
-//        instance.AutoSetAppProperties();
-//      }
-//      catch (Exception e)
-//      {
-//        Assert.Fail(e.Message);
-//      }
-//      Assert.Pass();
-//    }
-//  }
-//}
+﻿using System;
+using NUnit.Framework;
+using Foundation;
+using KissMetrics.iOS.Binding;
+
+namespace KissMetrics.iOS.Tests
+{
+  [TestFixture]
+  public class KissMetricsSDKTests
+  {
+    [Test]
+    public void SharedApi()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey");
+        var api = KISSmetricsAPI.SharedAPI;
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void SharedAPIWithKey()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void Identify()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Identify("identity");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void Identity()
+    {
+      try
+      {
+        var identity = KISSmetricsAPI.SharedAPIWithKey("apiKey").Identity;
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass(KISSmetricsAPI.SharedAPIWithKey("apiKey").Identity);
+    }
+
+    [Test]
+    public void ClearIdentity()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").ClearIdentity();
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void Alias()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Alias("Fidentity", "Sidentity");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void Record()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Record("name");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void RecordWithProperties()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Record("name", new NSDictionary());
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void RecordEvent()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").RecordEvent("name", new NSDictionary());
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void RecordWithPropertiesAndCondition()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Record("name", new NSDictionary(), KMARecordCondition.Always);
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void RecordWithCondition()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Record("name", KMARecordCondition.Always);
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void RecordOnce()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").RecordOnce("name");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void Set()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").Set(new NSDictionary());
+      }
+      catch(Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void SetProperties()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").SetProperties(new NSDictionary());
+      }
+      catch(Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void SetDistinct()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").SetDistinct(new NSObject(), "propertyKey");
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void AutoRecordAppLifecycle()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").AutoRecordAppLifecycle();
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void AutoRecordInstalls()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").AutoRecordInstalls();
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void AutoSetHardwareProperties()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").AutoSetHardwareProperties();
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+
+    [Test]
+    public void AutoSetAppProperties()
+    {
+      try
+      {
+        KISSmetricsAPI.SharedAPIWithKey("apiKey").AutoSetAppProperties();
+      }
+      catch (Exception e)
+      {
+        Assert.Fail(e.Message);
+      }
+      Assert.Pass();
+    }
+  }
+}
