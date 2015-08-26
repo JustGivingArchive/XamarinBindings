0bb1a0f97d3a2f40e4e769bb83e45c86c43c1301
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-08-26 15:19:11 +0300

    recreated bindings for kiss metrics

---
+++ KissMetrics.iOS/KissMetrics.iOS.Binding/StructsAndEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace KissMetrics.iOS.Binding
+{
+  [Native]
+  public enum KMARecordCondition : long
+  {
+    Always,
+    OncePerInstall,
+    OncePerIdentity
+  }
+}
+
