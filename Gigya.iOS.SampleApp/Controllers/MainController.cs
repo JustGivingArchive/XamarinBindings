437056b26937ea843d42d8aeccbba765d662bc50
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-09-04 15:54:49 +0300

    added requests

--- Gigya.iOS.SampleApp/Controllers/MainController.cs
+++ Gigya.iOS.SampleApp/Controllers/MainController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using UIKit;
 using MonoTouch.Dialog;
+using Foundation;
+using GigyaSDK.iOS;
 
 namespace Gigya.iOS.SampleApp
 {
@@ -14,8 +16,30 @@ namespace Gigya.iOS.SampleApp
         new Section("Login")
         {
           new StringElement("Login via Facebook", LoginViaFacebook),
+          new StringElement("Logout", Logout),          
+        },
+        new Section("Requests")
+        {
+          new StringElement("Request permissions", RequestPermissions),
+          new StringElement("Set status", SetStatus),
+          new StringElement("Get user info", GetUserInfo)
         }
       };
+    }
+
+    void Logout()
+    {
+      GigyaSDK.iOS.Gigya.LogoutWithCompletionHandler((responce, error) =>
+        {
+          if (error == null)
+          {
+          
+          }
+          else
+          {
+          
+          }
+        });
     }
 
     void LoginViaFacebook()
@@ -25,11 +49,75 @@ namespace Gigya.iOS.SampleApp
         {
           if (error != null)
           {
-          
+            Console.WriteLine(error.LocalizedDescription);
           }
           else
           {
-          
+            Console.WriteLine(user.Email);
+          }
+        });
+    }
+
+    void RequestPermissions()
+    {
+      // [Facebook SDK] Here you can check that token has all needed permissions
+      var t = Facebook.CoreKit.AccessToken.CurrentAccessToken;
+
+      GigyaSDK.iOS.Gigya.RequestNewFacebookPublishPermissions("publish_actions,publish_pages,manage_pages", (arg0, arg1, arg2) =>
+        {
+          if (arg0)
+          {
+            
+          }
+          else
+          {
+            
+          }
+        });
+      GigyaSDK.iOS.Gigya.RequestNewFacebookReadPermissions("email,user_status,contact_email,user_likes,user_location,user_friends,user_birthday,public_profile", (arg0, arg1, arg2) =>
+        {
+          if (arg0)
+          {
+
+          }
+          else
+          {
+
+          }
+        });      
+    }
+
+    void SetStatus()
+    {
+      Console.WriteLine(new GSSession().Token);
+
+      var request = GSRequest.RequestForMethod("socialize.setStatus");
+      request.Parameters.SetValueForKey(new NSString("I feel great"), new NSString("status"));
+      request.SendWithResponseHandler((responce, error) =>
+        {
+          if (error == null)
+          {
+            // Request was successful
+          }
+          else
+          {
+            // Handle error
+          }
+        });
+    }
+
+    void GetUserInfo()
+    {
+      var request = GSRequest.RequestForMethod("socialize.getUserInfo");
+      request.SendWithResponseHandler((responce, error) =>
+        {
+          if (error == null)
+          {
+            
+          }
+          else
+          {
+            
           }
         });
     }
