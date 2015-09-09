039f2c498647b4d3fb286f5aa9cc80fff08eb9a3
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-09-09 12:57:03 +0300

    added security framework to linkwith file

--- TrustDefender.iOS/TrustDefenderSDK.iOS/TrustDefenderMobile.linkwith.cs
+++ TrustDefender.iOS/TrustDefenderSDK.iOS/TrustDefenderMobile.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("TrustDefenderMobile.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("TrustDefenderMobile.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true, Frameworks = "Security")]
