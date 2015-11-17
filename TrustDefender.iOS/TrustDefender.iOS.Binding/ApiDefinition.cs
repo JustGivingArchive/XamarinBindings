506b8b504f798bb8c6a5761fb562af34535bd1ba
Uladzimir Adushkevich <uladzimir.adushkevich@instinctools.ru>
2015-11-17 13:37:46 +0300

    updated the ThreatMetrix binding to binary 3.1-57 version

--- TrustDefender.iOS/TrustDefender.iOS.Binding/ApiDefinition.cs
+++ TrustDefender.iOS/TrustDefender.iOS.Binding/ApiDefinition.cs
@@ -3,121 +3,120 @@ using Foundation;
 
 namespace TrustDefenderSDK.iOS
 {
-  [Static]
-  public partial interface Constants
-  {
-    // extern NSString *const TDMOrgID;
-    [Field("TDMOrgID", "__Internal")]
-    NSString TDMOrgID { get; }
+    [Static]
+    public partial interface Constants
+    {
+        // extern NSString *const TDMOrgID __attribute__((visibility("default")));
+        [Field("TDMOrgID", "__Internal")]
+        NSString TDMOrgID { get; }
 
-    // extern NSString *const TDMApiKey;
-    [Field("TDMApiKey", "__Internal")]
-    NSString TDMApiKey { get; }
+        // extern NSString *const TDMApiKey __attribute__((visibility("default")));
+        [Field("TDMApiKey", "__Internal")]
+        NSString TDMApiKey { get; }
 
-    // extern NSString *const TDMDelegate;
-    [Field("TDMDelegate", "__Internal")]
-    NSString TDMDelegate { get; }
+        [Field("TDMDelegate", "__Internal")]
+        NSString TDMDelegate { get; }
 
-    // extern NSString *const TDMTimeout;
-    [Field("TDMTimeout", "__Internal")]
-    NSString TDMTimeout { get; }
-    
-    // extern NSString *const TDMLocationServices;
-    [Field("TDMLocationServices", "__Internal")]
-    NSString TDMLocationServices { get; }
-    
-    // extern NSString *const TDMLocationServicesWithPrompt;
-    [Field("TDMLocationServicesWithPrompt", "__Internal")]
-    NSString TDMLocationServicesWithPrompt { get; }
-    
-    // extern NSString *const TDMDesiredLocationAccuracy;
-    [Field("TDMDesiredLocationAccuracy", "__Internal")]
-    NSString TDMDesiredLocationAccuracy { get; }
-    
-    // extern NSString *const TDMKeychainAccessGroup;
-    [Field("TDMKeychainAccessGroup", "__Internal")]
-    NSString TDMKeychainAccessGroup { get; }
-    
-    // extern NSString *const TDMOptions;
-    [Field("TDMOptions", "__Internal")]
-    NSString TDMOptions { get; }
-    
-    // extern NSString *const TDMFingerprintServer;
-    [Field("TDMFingerprintServer", "__Internal")]
-    NSString TDMFingerprintServer { get; }
-    
-    // extern NSString *const TDMProfileSourceURL;
-    [Field("TDMProfileSourceURL", "__Internal")]
-    NSString TDMProfileSourceURL { get; }
-    
-    // extern NSString *const TDMSessionID;
-    [Field("TDMSessionID", "__Internal")]
-    NSString TDMSessionID { get; }
-    
-    // extern NSString *const TDMCustomAttributes;
-    [Field("TDMCustomAttributes", "__Internal")]
-    NSString TDMCustomAttributes { get; }
-    
-    // extern NSString *const TDMLocation;
-    [Field("TDMLocation", "__Internal")]
-    NSString TDMLocation { get; }
-    
-    // extern NSString *const TDMProfileStatus;
-    [Field("TDMProfileStatus", "__Internal")]
-    NSString TDMProfileStatus { get; }
-  }
+        // extern NSString *const TDMTimeout __attribute__((visibility("default")));
+        [Field("TDMTimeout", "__Internal")]
+        NSString TDMTimeout { get; }
 
-  // @interface TrustDefenderMobile : NSObject
-  [BaseType(typeof(NSObject))]
-  interface TrustDefenderMobile
-  {
-    // -(id)initWithConfig:(NSDictionary *)config;
-    [Export("initWithConfig:")]
-    IntPtr Constructor(NSDictionary config);
+        // extern NSString *const TDMLocationServices __attribute__((visibility("default")));
+        [Field("TDMLocationServices", "__Internal")]
+        NSString TDMLocationServices { get; }
 
-    // -(THMStatusCode)doProfileRequest;
-    [Export("doProfileRequest")]
-    //[Verify(MethodToProperty)]
-    THMStatusCode DoProfileRequest();
+        // extern NSString *const TDMLocationServicesWithPrompt __attribute__((visibility("default")));
+        [Field("TDMLocationServicesWithPrompt", "__Internal")]
+        NSString TDMLocationServicesWithPrompt { get; }
 
-    // -(THMStatusCode)doProfileRequest:(NSDictionary *)options;
-    [Export("doProfileRequest:")]
-    THMStatusCode DoProfileRequest(NSDictionary options);
+        // extern NSString *const TDMDesiredLocationAccuracy __attribute__((visibility("default")));
+        [Field("TDMDesiredLocationAccuracy", "__Internal")]
+        NSString TDMDesiredLocationAccuracy { get; }
 
-    // -(THMStatusCode)doProfileRequestWithCallback:(void (^)(NSDictionary *))callbackBlock;
-    [Export("doProfileRequestWithCallback:")]
-    THMStatusCode DoProfileRequestWithCallback(Action<NSDictionary> callbackBlock);
+        // extern NSString *const TDMKeychainAccessGroup __attribute__((visibility("default")));
+        [Field("TDMKeychainAccessGroup", "__Internal")]
+        NSString TDMKeychainAccessGroup { get; }
 
-    // -(THMStatusCode)doProfileRequestWithOptions:(NSDictionary *)profileOptions andCallbackBlock:(void (^)(NSDictionary *))callbackBlock;
-    [Export("doProfileRequestWithOptions:andCallbackBlock:")]
-    THMStatusCode DoProfileRequestWithOptions(NSDictionary profileOptions, Action<NSDictionary> callbackBlock);
+        // extern NSString *const TDMOptions __attribute__((visibility("default")));
+        [Field("TDMOptions", "__Internal")]
+        NSString TDMOptions { get; }
 
-    // -(void)pauseLocationServices:(BOOL)pause;
-    [Export("pauseLocationServices:")]
-    void PauseLocationServices(bool pause);
+        // extern NSString *const TDMFingerprintServer __attribute__((visibility("default")));
+        [Field("TDMFingerprintServer", "__Internal")]
+        NSString TDMFingerprintServer { get; }
 
-    // -(NSDictionary *)getResult;
-    [Export("getResult")]
-    //[Verify(MethodToProperty)]
-    NSDictionary Result { get; }
+        // extern NSString *const TDMProfileSourceURL __attribute__((visibility("default")));
+        [Field("TDMProfileSourceURL", "__Internal")]
+        NSString TDMProfileSourceURL { get; }
 
-    // -(NSString *)version;
-    [Export("version")]
-    //[Verify(MethodToProperty)]
-    string Version { get; }
+        // extern NSString *const TDMSessionID __attribute__((visibility("default")));
+        [Field("TDMSessionID", "__Internal")]
+        NSString TDMSessionID { get; }
 
-    // -(void)cancel;
-    [Export("cancel")]
-    void Cancel();
-  }
+        // extern NSString *const TDMCustomAttributes __attribute__((visibility("default")));
+        [Field("TDMCustomAttributes", "__Internal")]
+        NSString TDMCustomAttributes { get; }
 
-  // @protocol TrustDefenderMobileDelegate
-  [Protocol, Model]
-  interface TrustDefenderMobileDelegate
-  {
-    // @required -(void)profileComplete:(NSDictionary *)profileResults;
-    [Abstract]
-    [Export("profileComplete:")]
-    void ProfileComplete(NSDictionary profileResults);
-  }
+        // extern NSString *const TDMLocation __attribute__((visibility("default")));
+        [Field("TDMLocation", "__Internal")]
+        NSString TDMLocation { get; }
+
+        // extern NSString *const TDMProfileStatus __attribute__((visibility("default")));
+        [Field("TDMProfileStatus", "__Internal")]
+        NSString TDMProfileStatus { get; }
+    }
+
+    // @interface TrustDefenderMobile : NSObject
+    [BaseType(typeof(NSObject))]
+    interface TrustDefenderMobile
+    {
+        // -(id)initWithConfig:(NSDictionary *)config;
+        [Export("initWithConfig:")]
+        IntPtr Constructor(NSDictionary config);
+
+        // -(THMStatusCode)doProfileRequest;
+        [Export("doProfileRequest")]
+        //        [Verify (MethodToProperty)]
+        THMStatusCode DoProfileRequest();
+
+        // -(THMStatusCode)doProfileRequest:(NSDictionary *)options;
+        [Export("doProfileRequest:")]
+        THMStatusCode DoProfileRequest(NSDictionary options);
+
+        // -(THMStatusCode)doProfileRequestWithCallback:(void (^)(NSDictionary *))callbackBlock;
+        [Export("doProfileRequestWithCallback:")]
+        THMStatusCode DoProfileRequestWithCallback(Action<NSDictionary> callbackBlock);
+
+        // -(THMStatusCode)doProfileRequestWithOptions:(NSDictionary *)profileOptions andCallbackBlock:(void (^)(NSDictionary *))callbackBlock;
+        [Export("doProfileRequestWithOptions:andCallbackBlock:")]
+        THMStatusCode DoProfileRequestWithOptions(NSDictionary profileOptions, Action<NSDictionary> callbackBlock);
+
+        // -(void)pauseLocationServices:(BOOL)pause;
+        [Export("pauseLocationServices:")]
+        void PauseLocationServices(bool pause);
+
+        // -(NSDictionary *)getResult;
+        [Export("getResult")]
+        //        [Verify (MethodToProperty)]
+        NSDictionary Result { get; }
+
+        // -(NSString *)version;
+        [Export("version")]
+        //        [Verify (MethodToProperty)]
+        string Version { get; }
+
+        // -(void)cancel;
+        [Export("cancel")]
+        void Cancel();
+    }
+
+    // @protocol TrustDefenderMobileDelegate
+    [Protocol, Model]
+    interface TrustDefenderMobileDelegate
+    {
+        // @required -(void)profileComplete:(NSDictionary *)profileResults;
+        [Abstract]
+        [Export("profileComplete:")]
+        void ProfileComplete(NSDictionary profileResults);
+    }
 }
--- TrustDefender.iOS/TrustDefender.iOS.Binding/StructsAndEnums.cs
+++ TrustDefender.iOS/TrustDefender.iOS.Binding/StructsAndEnums.cs
@@ -2,18 +2,18 @@
 
 namespace TrustDefenderSDK.iOS
 {
-  [Native]
-  public enum THMStatusCode : long
-  {
-    NotYet = 0,
-    Ok,
-    ConnectionError,
-    HostNotFoundError,
-    NetworkTimeoutError,
-    HostVerificationError,
-    InternalError,
-    InterruptedError,
-    PartialProfile,
-    InvalidOrgID
-  }
+    [Native]
+    public enum THMStatusCode : long
+    {
+        NotYet = 0,
+        Ok,
+        ConnectionError,
+        HostNotFoundError,
+        NetworkTimeoutError,
+        HostVerificationError,
+        InternalError,
+        InterruptedError,
+        PartialProfile,
+        InvalidOrgID
+    }
 }
